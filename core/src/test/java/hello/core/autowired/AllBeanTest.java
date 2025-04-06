e5301890edbc153fe043ba26573193ae03b44e60
dev-cyan <zyos1@naver.com>
2025-04-06 14:52:01 +0900

    빈 생명주기 콜백

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
